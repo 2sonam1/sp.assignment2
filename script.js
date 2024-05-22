1dde417dc94f3446f06e8b225c24d13969ecd4ed
2sonam1 <sonam.rani.min20@itbhu.ac.in>
2024-05-23 03:46:25 +0530

    changes in header

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
     $(".drop-open button").click(function () {
-        $(".nav-links").css({ "visibility": "visible", "opacity": "1", "height": "auto", "padding":"30px 0 30px 15px"});
-        $(".drop-open button").css({ "visibility": "hidden", "opacity" :"0"})
-        $(".drop-close button").css({ "visibility": "visible", "opacity": "1" })
+        $(".nav-links").addClass("active");
+        $(".drop-open button").addClass("close")
+        $(".drop-close button").addClass("open")
     });
     $(".drop-close button").click(function () {
-        $(".nav-links").css({ "visibility": "hidden", "opacity": "0", "height": "0", "padding": "0" });
-        $(".drop-open button").css({ "visibility": "visible", "opacity": "1" })
-        $(".drop-close button").css({ "visibility": "hidden", "opacity": "0" })
+        $(".nav-links").removeClass("active")
+        $(".drop-open button").removeClass("close")
+        $(".drop-close button").removeClass("open")
     });
 });
