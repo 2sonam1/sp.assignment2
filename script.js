c841189909a45ef6c715cf9782f19f3fdfcddbb5
2sonam1 <sonam.rani.min20@itbhu.ac.in>
2024-05-29 13:19:11 +0530

    completed from my side

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
+        let obj = document.getElementById(id),
+            current = start,
+            range = end - start,
+            increment = end > start ? 1 : -1,
+            step = Math.abs(Math.floor(duration / range)),
+            timer = setInterval(() => {
+                current += increment;
+                obj.textContent = current;
+                if (current == end) {
+                    clearInterval(timer);
+                }
+            }, step);
+    }
+    counter("count1", 0, 40, 3000);
+    counter("count2", 0, 60, 2500);
+    counter("count3", 0, 50, 3000);
+});
 
 $(document).ready(function () {
     $(".drop-open button").click(function () {
@@ -11,4 +29,7 @@ $(document).ready(function () {
         $(".drop-open button").removeClass("close")
         $(".drop-close button").removeClass("open")
     });
+    $(".dropdwn > li").hover(function(){
+        $(this).children("a").toggleClass("active").siblings("ul").toggleClass("open")
+    }) 
 });
